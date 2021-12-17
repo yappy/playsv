ecfa04b97c589ae067d083639419a697480f0b46
yappy <superunyo@yahoo.co.jp>
2021-12-18 05:54:37 +0900

    Round state and haipai

--- playsv/src/mj.rs
+++ playsv/src/mj.rs
@@ -42,9 +42,13 @@ struct GameState {
     player_count: u32,
     round_max: u32,
     // variable
-    round: u32,
+    // wind 0..round_max, parent 0..player_count, hon
+    wind: u32,
+    parent: u32,
+    hon: u32,
     points: [u32; 4],
     yama: VecDeque<u8>,
+    turn: u32,
     hands: [Vec<u8>; 4],
 }
 
@@ -90,11 +94,22 @@ impl GameState {
         GameState {
             player_count: 0,
             round_max: 0,
-            round: 0,
+            wind: 0,
+            parent: 0,
+            hon: 0,
             points: Default::default(),
             yama: VecDeque::new(),
+            turn: 0,
             hands: Default::default(),
         }
+    }
+
+    fn check(&self) {
+        assert!(2 >= self.player_count && self.player_count <= 4);
+        assert!(self.round_max <= 4);
+        assert!(self.wind < 4);
+        assert!(self.parent < self.player_count);
+        assert!(self.turn < self.player_count);
     }
 
     fn init(&mut self) -> Result<(), String> {
@@ -102,23 +117,32 @@ impl GameState {
         self.player_count = 2;
         let player_size = self.player_count as usize;
         self.round_max = 4;
-        self.round = 1;
+        self.wind = 0;
+        self.parent = 0;
+        self.hon = 0;
         for (i, p) in self.points.iter_mut().enumerate() {
             *p = if i < player_size { 25000 } else { 0 };
         }
         self.yama.clear();
+        self.turn = 0;
         for hand in self.hands.iter_mut() {
             hand.clear();
         }
 
-        // init as Round 1 start state
-        self.next_round(1);
+        // init as tong 1 kyoku 0 hon start
+        self.next_round(0, 0, 0);
+
+        self.check();
 
         Ok(())
     }
 
-    fn next_round(&mut self, round: u32) {
-        self.round = round;
+    fn next_round(&mut self, wind: u32, parent: u32, hon: u32) {
+        self.wind = wind;
+        self.parent = parent;
+        self.hon = hon;
+        // the first turn player == parent == kyoku number
+        self.turn = parent;
         // Create yama
         {
             let mut yama_tmp: Vec<u8> = vec![];
@@ -139,6 +163,25 @@ impl GameState {
 
             self.yama = yama_tmp.into();
         }
-        // TODO: haipai
+        // haipai
+        {
+            // 4 times
+            for count in 0..4 {
+                // parent-origin, for each player
+                for i in 0..self.player_count {
+                    let player = (parent + i) % self.player_count;
+                    // take 4, 4, 4, 1
+                    let take_num = if count == 3 { 1 } else { 4 };
+                    for _ in 0..take_num {
+                        let hai = self.yama.pop_back().unwrap();
+                        self.hands[player as usize].push(hai);
+                    }
+                }
+            }
+            for i in 0..self.player_count {
+                assert!(self.hands[i as usize].len() == 13);
+            }
+        }
+        self.check();
     }
 }
