7f591c0ee412918a0b5559e7b6668f5e0c38fa79
yappy <superunyo@yahoo.co.jp>
2023-09-24 02:35:20 +0900

    Simple wasm app

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,18 @@
+use web_sys::window;
+
+fn app_main() {
+    let document = window().and_then(|win| win.document()).expect("Could not access document");
+    let body = document.body().expect("Could not access document.body");
+    let text_node = document.create_text_node("Hello, world from Vanilla Rust!");
+    body.append_child(text_node.as_ref()).expect("Failed to append text");
+}
+
+fn init() {
+    console_error_panic_hook::set_once();
+    wasm_logger::init(wasm_logger::Config::default());
+}
+
 fn main() {
-    //println!("Hello, world!");
+    init();
+    app_main();
 }
