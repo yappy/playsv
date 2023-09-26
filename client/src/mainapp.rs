63a7db607287c40cc8c7e223e76d8267776c069a
yappy <superunyo@yahoo.co.jp>
2023-09-27 02:42:57 +0900

    Command hash map

--- client/src/mainapp.rs
+++ client/src/mainapp.rs
@@ -1,17 +1,82 @@
+use std::{collections::HashMap, rc::Rc};
+
 use crate::basesys::{App, BaseSys};
+use getopts::{Matches, Options};
 use web_sys::CanvasRenderingContext2d;
 
 const CANVAS_W: u32 = 640;
 const CANVAS_H: u32 = 480;
 
+type DbgCmdFunc = dyn Fn(&mut MainApp, Matches);
+struct DbgCmd {
+    opts: Options,
+    func: Rc<Box<DbgCmdFunc>>,
+}
+
 struct MainApp {
     frame: u64,
+
+    dbg_cmds: HashMap<&'static str, DbgCmd>,
 }
 
 impl MainApp {
     fn new() -> Self {
-        Self { frame: 0 }
+        let dbg_cmds = Self::create_dbg_cmds();
+
+        Self { frame: 0, dbg_cmds }
     }
+
+    fn exec_dbg_cmd(&mut self, cmd: &str, args: &str) {
+        let func;
+        let matches;
+        if let Some(v) = self.dbg_cmds.get(cmd) {
+            match v.opts.parse(args.split_ascii_whitespace()) {
+                Ok(m) => {
+                    func = v.func.clone();
+                    matches = m;
+                }
+                Err(e) => {
+                    log::error!("{e}");
+                    return;
+                }
+            }
+        } else {
+            log::error!("Command not found: {cmd}");
+            return;
+        }
+        func(self, matches);
+    }
+
+    fn create_dbg_cmds() -> HashMap<&'static str, DbgCmd> {
+        let mut dbg_cmds = HashMap::new();
+
+        let mut opts = Options::new();
+        opts.optflag("h", "help", "Print help");
+        dbg_cmds.insert(
+            "help",
+            DbgCmd {
+                opts,
+                func: Rc::new(Box::new(Self::dbg_help)),
+            },
+        );
+
+        let mut opts = Options::new();
+        opts.optflag("h", "help", "Print help");
+        dbg_cmds.insert(
+            "echo",
+            DbgCmd {
+                opts,
+                func: Rc::new(Box::new(Self::dbg_echo)),
+            },
+        );
+
+        dbg_cmds
+    }
+
+    fn dbg_help(&mut self, _args: Matches) {
+        log::debug!("HELP OK");
+    }
+    fn dbg_echo(&mut self, _args: Matches) {}
 }
 
 impl App for MainApp {
@@ -35,6 +100,7 @@ impl App for MainApp {
             (cmdline, "")
         };
         log::info!("cmd: {cmd}, args: {args}");
+        self.exec_dbg_cmd(cmd, args);
     }
 }
 
