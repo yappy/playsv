165eb2ffb1db39e34bc0b36af5fd54a3363c12eb
yappy <superunyo@yahoo.co.jp>
2023-09-29 04:16:27 +0900

    Add user function

--- client/build.rs
+++ client/build.rs
@@ -1,22 +1,39 @@
-use std::{env, path::Path, fs};
 use anyhow::Result;
+use base64::{engine::general_purpose, Engine as _};
 use glob::glob;
+use std::{env, fs, path::Path};
+
+const RS_HEADER: &str = "
+fn init_assets() -> HashMap<&'static str, &'static str> {
+    let mut map = HashMap::new();
+
+";
+
+const RS_FOOTER: &str = "
+    map
+}
+";
 
 fn process_assets(out_dir: &Path) -> Result<()> {
-    let src_dir = "src/asset";
-    let pattern = format!("{src_dir}/**/*.*");
+    let src_dir = "src/asset/";
+    let pattern = format!("{src_dir}**/*.*");
     let dst_path = out_dir.join("asset.rs");
-    let mut asset_rs = String::new();
+    let mut asset_rs = String::from(RS_HEADER);
 
     println!("cargo:rerun-if-changed={src_dir}");
 
     for entry in glob(&pattern)? {
         let entry = entry?;
         let name = entry.to_str().expect("Invalid path string");
-        println!("{name}");
+        let name = &name[src_dir.len()..];
+
+        let bin = fs::read(&entry)?;
+        let encoded = general_purpose::STANDARD_NO_PAD.encode(bin);
+
+        asset_rs += &format!("    map.insert(\"{}\", \"{}\");\n", name, encoded);
     }
 
-    asset_rs += "// hello!\n";
+    asset_rs += RS_FOOTER;
     fs::write(&dst_path, &asset_rs)?;
     println!("{}", out_dir.to_string_lossy());
 
--- client/src/asset.rs
+++ client/src/asset.rs
@@ -1,1 +1,15 @@
+use anyhow::{anyhow, Result};
+use std::collections::HashMap;
+use std::sync::OnceLock;
+
+// Built by build.rs
 include!(concat!(env!("OUT_DIR"), "/asset.rs"));
+
+pub fn read_file(path: &str) -> Result<&'static str> {
+    static FS: OnceLock<HashMap<&str, &str>> = OnceLock::new();
+    let map = FS.get_or_init(init_assets);
+
+    map.get(path)
+        .copied()
+        .ok_or(anyhow!("Not found: {path}"))
+}
