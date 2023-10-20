bc51dacb626a7b265bcaae7a73cb9e77081dd7df
yappy <superunyo@yahoo.co.jp>
2023-10-21 03:48:18 +0900

    Shanten chitoi

--- game/src/mjsys/shanten.rs
+++ game/src/mjsys/shanten.rs
@@ -1,24 +1,28 @@
 use super::{Hand, PAI_COUNT_U8};
 
-pub fn all(hand: &Hand) -> u32 {
+fn hand_check(hand: &Hand) {
     let count = super::bucket_count(&hand.bucket);
     let fulou = hand.mianzi_list.len() as u32 * 3;
-    assert!(count + fulou == 13);
 
+    assert!(count + fulou == 13);
+}
+
+pub fn all(hand: &Hand) -> u32 {
     let s1 = kokushi(hand);
     if s1 == 0 {
         return 0;
     }
-    let s2 = chitoi();
+    let s2 = chitoi(hand);
     if s2 == 0 {
         return 0;
     }
-    let s3 = normal();
+    let s3 = normal(hand);
 
     s1.min(s2).min(s3)
 }
 
-fn kokushi(hand: &Hand) -> u32 {
+pub fn kokushi(hand: &Hand) -> u32 {
+    hand_check(hand);
     if !hand.mianzi_list.is_empty() {
         return u32::MAX;
     }
@@ -42,11 +46,30 @@ fn kokushi(hand: &Hand) -> u32 {
     13 - count
 }
 
-fn chitoi() -> u32 {
-    todo!()
+pub fn chitoi(hand: &Hand) -> u32 {
+    hand_check(hand);
+    if !hand.mianzi_list.is_empty() {
+        return u32::MAX;
+    }
+
+    let mut kind = 0;
+    let mut dual = 0;
+    for pai in 0..PAI_COUNT_U8 {
+        if hand.bucket[pai as usize] > 0 {
+            kind += 1;
+        }
+        if hand.bucket[pai as usize] >= 2 {
+            dual += 1;
+        }
+    }
+
+    assert!(dual <= 6);
+    // increase if kind is less than 7
+    6 - dual + 7u32.saturating_sub(kind)
 }
 
-fn normal() -> u32 {
+pub fn normal(hand: &Hand) -> u32 {
+    hand_check(hand);
     todo!()
 }
 
@@ -69,6 +92,21 @@ mod tests {
         assert_eq!(1, kokushi(&hand));
         let hand = from_human_readable_string("2222333344445m")?;
         assert_eq!(13, kokushi(&hand));
+
+        Ok(())
+    }
+
+    #[test]
+    fn chitoi_only() -> Result<()> {
+        //https://mahjong.ara.black/etc/shanten/shanten2.htm
+        let hand = from_human_readable_string("58m667p116688s55z")?;
+        assert_eq!(1, chitoi(&hand));
+        let hand = from_human_readable_string("5m6666p116688s55z")?;
+        assert_eq!(2, chitoi(&hand));
+        let hand = from_human_readable_string("444m6666p1111s55z")?;
+        assert_eq!(5, chitoi(&hand));
+        let hand = from_human_readable_string("1m9m1p9p1s9s1234567z")?;
+        assert_eq!(6, chitoi(&hand));
 
         Ok(())
     }
