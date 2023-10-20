949418e6c578ec57a5acde0430fd7e2e24fc042a
yappy <superunyo@yahoo.co.jp>
2023-10-21 04:35:00 +0900

    Add heavy full shanten tests
    They are heavy a little.
    `cargo test --release` might be better.

--- game/src/mjsys/shanten.rs
+++ game/src/mjsys/shanten.rs
@@ -75,10 +75,11 @@ pub fn normal(hand: &Hand) -> u32 {
 
 #[cfg(test)]
 mod tests {
-    use anyhow::Result;
-
     use super::super::*;
     use super::*;
+    use anyhow::Result;
+    use std::fs;
+    use std::path::PathBuf;
 
     #[test]
     fn kokushi_only() -> Result<()> {
@@ -107,6 +108,57 @@ mod tests {
         assert_eq!(5, chitoi(&hand));
         let hand = from_human_readable_string("1m9m1p9p1s9s1234567z")?;
         assert_eq!(6, chitoi(&hand));
+
+        Ok(())
+    }
+
+    #[test]
+    fn full() -> Result<()> {
+        fn process_line(nums: &[i8], filename: &str, lineno: usize) {
+            let hand14 = &nums[0..14];
+            // if agari, -1: for all hand13, shanten = 0
+            //let exp_n = nums[14].max(0) as u32;
+            let exp_k = nums[15].max(0) as u32;
+            let exp_c = nums[16].max(0) as u32;
+
+            //let mut n_min = u32::MAX;
+            let mut k_min = u32::MAX;
+            let mut c_min = u32::MAX;
+            for del in 0..14 {
+                let mut hand: Hand = Default::default();
+                for i in 0..14 {
+                    if i != del {
+                        hand.bucket[hand14[i] as usize] += 1;
+                    }
+                }
+                //let n_min = normal(&hand).min(n_min);
+                k_min = kokushi(&hand).min(k_min);
+                c_min = chitoi(&hand).min(c_min);
+            }
+            assert_eq!(exp_k, k_min, "{filename}:{lineno}");
+            assert_eq!(exp_c, c_min, "{filename}:{lineno}");
+        }
+
+        fn process_file(filename: &str) -> Result<()> {
+            let path: PathBuf = [env!("CARGO_MANIFEST_DIR"), "testres", filename]
+                .iter()
+                .collect();
+            for (no, line) in fs::read_to_string(&path)?.lines().enumerate() {
+                let mut nums = [0i8; 17];
+                line.split_ascii_whitespace()
+                    .enumerate()
+                    .for_each(|(i, tok)| nums[i] = tok.parse::<i8>().unwrap());
+
+                process_line(&nums, filename, no + 1)
+            }
+
+            Ok(())
+        }
+
+        process_file("p_normal_10000.txt").unwrap();
+        process_file("p_hon_10000.txt").unwrap();
+        process_file("p_tin_10000.txt").unwrap();
+        process_file("p_koku_10000.txt").unwrap();
 
         Ok(())
     }
